03a09126999e5c284f58e6f6735ace4ef5c74c7e
Marek Wodzinski <majek@w7i.pl>
2025-11-25 00:30:47 +0100

    License change, adapt script to Cura guidelines.

--- README.md
+++ README.md
@@ -37,14 +37,14 @@ Make sure 'Enable Bridge Settings' and 'Bridge Fan Speed' are enabled in Cura's
 Copyright (c) 2025 Marek Wodzinski
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
--- SpinUpFanBeforeBridge.py
+++ SpinUpFanBeforeBridge.py
@@ -1,27 +1,45 @@
-# Copyright (c) 2025 Marek Wodzinski
+# Copyright (C) 2025 Marek Wodzinski
 #
-# Script for Cura slicer to spin-up part cooling fan before bridge.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Cura can control/enable cooling fan while printing bridges. But more powerful
-# fans don't start instantly and often printer is in the middle of printing
-# bridge when fan spin up to target speed. Solution for this is to spin-up fan
-# before hotend starts printing bridge. With this plugin it's possible to start
-# fan earlier by defined time. For simplicity, there is independent setting
-# for fan speed - just set it to the same value as in 'Bridge Fan Speed',
-# but it can be set to any other value, for example lower to gradualy spin-up,
-# or higher to 'kick' fan as fast as possible, but use lower defined speed
-# on bridges.
-# 
-
-# Implements a sliding time-window to insert fan commands exactly X seconds 
-# before a bridge, regardless of layer changes or command complexity.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+#
+# Script for Cura slicer to spin up the part-cooling fan before printing bridge.
+#
+# Cura can enable and control cooling fan when printing bridges, but more powerful
+# fans don't start instantly. Often, the printer is already in the middle of printing
+# the bridge by the time the fan reaches the target speed.
+#
+# The solution is to spin up fan before the hotend starts printing the bridge.
+# With this script it's possible to start fan earlier by defined amount of time.
+#
 
 from ..Script import Script
 import math
 from collections import deque
 
 class SpinUpFanBeforeBridge(Script):
+    """
+    Implements a sliding time-window to insert fan commands exactly X seconds
+    before a bridge, regardless of layer changes or command complexity.
+    """
+
     def getSettingDataString(self):
+        """
+        Provides the JSON setting definitions for the script.
+        
+        :return: A string containing the JSON setting definitions.
+        """
         return """{
             "name": "Spin Up Fan Before Bridge",
             "key": "SpinUpFanBeforeBridge",
@@ -32,7 +50,7 @@ class SpinUpFanBeforeBridge(Script):
                 "lead_time":
                 {
                     "label": "Lead Time (seconds)",
-                    "description": "Time offset to start the fan before the bridge.",
+                    "description": "How many seconds before the bridge should the fan be turned on? This compensates for the time it takes for the fan to reach full speed.",
                     "type": "float",
                     "default_value": 1.5,
                     "unit": "s"
@@ -40,7 +58,7 @@ class SpinUpFanBeforeBridge(Script):
                 "target_fan_speed":
                 {
                     "label": "Bridge Fan Speed %",
-                    "description": "Target fan speed to insert (0-100).",
+                    "description": "Set the desired fan speed for the spin-up command (0-100). For consistent cooling, this should match the 'Bridge Fan Speed' setting in Cura.",
                     "type": "int",
                     "default_value": 100,
                     "unit": "%"
@@ -48,13 +66,19 @@ class SpinUpFanBeforeBridge(Script):
             }
         }"""
 
-    def get_val(self, line, key):
-        # Fast non-regex parsing
+    def _get_value(self, line, key):
+        """
+        Fast, non-regex G-code parameter parsing.
+        
+        :param line: The G-code line to parse.
+        :param key: The parameter key (e.g., 'X', 'Y', 'F').
+        :return: The floating point value of the parameter, or None if not found.
+        """
         if key not in line:
             return None
         try:
             start = line.find(key) + 1
-            # Find the end of the number (space or semicolon or end of line)
+            # Find the end of the number (space, semicolon, or end of line)
             end = start
             l_len = len(line)
             while end < l_len:
@@ -67,24 +91,37 @@ class SpinUpFanBeforeBridge(Script):
             return None
 
     def execute(self, data):
+        """
+        Main execution method that processes the G-code data.
+
+        :param data: A list of strings, where each string is a layer of G-code.
+        :return: The modified list of G-code strings.
+        """
         lead_time = self.getSettingValueByKey("lead_time")
-        fan_pwm = int(self.getSettingValueByKey("target_fan_speed") * 2.55)
+        target_fan_speed = self.getSettingValueByKey("target_fan_speed")
+        
+        # Convert fan speed percentage (0-100) to PWM value (0-255)
+        fan_pwm = int((target_fan_speed / 100.0) * 255.0)
         fan_cmd = f"M106 S{fan_pwm} ; SpinUpFanBeforeBridge (Lead: {lead_time}s)"
 
-        # State Tracking
+        # --- State Tracking ---
+        # Tracks the extruder's current position and state.
         current_x = 0.0
         current_y = 0.0
-        current_f = 3000.0  # Default feedrate (mm/min) if none found early
-        total_time = 0.0
-        is_relative = False # Default to absolute positioning
+        current_f = 3000.0  # Assumed default feedrate (mm/min).
+        total_time = 0.0    # Total elapsed print time in seconds.
+        is_relative = False # Tracks G90 (absolute) vs G91 (relative) mode.
 
-        # The Sliding Window
-        # Stores tuples: (timestamp, layer_index, line_index)
-        # timestamp represents the time at the START of that line's execution
+        # --- The Sliding Window ---
+        # A deque that stores recent G-code commands as tuples:
+        # (timestamp, layer_index, line_index)
+        # 'timestamp' is the total elapsed time at the START of that command.
         history = deque()
         
-        # We collect insertions to apply them after the single pass
-        # Tuple: (layer_index, line_index, string_to_insert)
+        # --- Insertions ---
+        # A list to collect all commands to be inserted. We apply them in a
+        # separate pass to avoid modifying the list while iterating.
+        # Tuple format: (layer_index, line_index, string_to_insert)
         insertions = []
 
         # We split data once to make it addressable, but we keep the structure
@@ -93,7 +130,7 @@ class SpinUpFanBeforeBridge(Script):
 
         for layer_idx, lines in enumerate(layers_lines):
             for line_idx, line in enumerate(lines):
-                # Check for coordinate system changes
+                # Check for coordinate system changes (modal)
                 if "G90" in line:
                     is_relative = False
                 if "G91" in line:
@@ -101,102 +138,94 @@ class SpinUpFanBeforeBridge(Script):
 
                 # 1. Check for Bridge Trigger
                 if ";BRIDGE" in line:
-                    # We need to insert the fan command at (total_time - lead_time)
+                    # Calculate the target time to insert the fan command.
                     target_time = total_time - lead_time
                     
-                    # Find the insertion point in our history buffer
-                    # Since history is sorted by time, we can find the spot efficiently
+                    # Find the correct insertion point in our history buffer.
                     if len(history) > 0:
-                        # We want the first entry where timestamp >= target_time
-                        # Optimization: Check the ends first
+                        # Optimization: Check the boundaries first.
                         if target_time <= history[0][0]:
-                            # Request is older than our history (start of print?)
+                            # Target time is before our recorded history; insert at the very beginning.
                             insertions.append((history[0][1], history[0][2], fan_cmd))
                         elif target_time >= history[-1][0]:
-                            # Request is in the future? (Should be impossible for lead time)
+                            # This case should ideally not happen with a lead time.
+                            # It means the insertion point is the current line.
                             insertions.append((layer_idx, line_idx, fan_cmd))
                         else:
-                            # Binary search / Linear scan for the closest point
-                            # Since deque isn't indexable for bisect, we iterate backwards or convert.
-                            # Given lead_time is usually small (1-5s), linear scan from right is fast.
+                            # Scan backwards from the most recent entry for efficiency,
+                            # as the target is likely to be close.
                             found = False
                             for i in range(len(history) - 1, -1, -1):
-                                t, l_i, ln_i = history[i]
+                                t, hist_layer_idx, hist_line_idx = history[i]
                                 if t <= target_time:
-                                    # We found the point just BEFORE our target time.
-                                    # So we insert at the *next* instruction (history[i+1])
-                                    # or just use this one as close enough.
-                                    # Let's insert at the point immediately following this timestamp
+                                    # We found the command that executes *just before* our target time.
+                                    # We must insert the fan command *after* it.
                                     if i + 1 < len(history):
                                         ins_l, ins_ln = history[i+1][1], history[i+1][2]
                                         insertions.append((ins_l, ins_ln, fan_cmd))
-                                    else:
-                                        insertions.append((l_i, ln_i, fan_cmd))
+                                    else: # Should not happen if boundary checks are correct
+                                        insertions.append((hist_layer_idx, hist_line_idx, fan_cmd))
                                     found = True
                                     break
                             
                             if not found:
-                                # Fallback to oldest history
+                                # Fallback: If no suitable point is found, insert at the oldest history entry.
                                 insertions.append((history[0][1], history[0][2], fan_cmd))
 
                 # 2. Parse Movement & Accumulate Time
                 if "G1" in line or "G0" in line:
-                    # Store the state BEFORE this move executes (Insertion Point)
-                    # We append to history BEFORE updating time, because if we insert here,
-                    # it runs before this move.
+                    # Append current state to history BEFORE calculating this move's time.
                     history.append((total_time, layer_idx, line_idx))
 
-                    # Parse Parameters
-                    x = self.get_val(line, "X")
-                    y = self.get_val(line, "Y")
-                    f = self.get_val(line, "F")
+                    # Parse G-code parameters
+                    x = self._get_value(line, "X")
+                    y = self._get_value(line, "Y")
+                    f = self._get_value(line, "F")
 
-                    # Update Feedrate if present (Modal)
+                    # Feedrate is modal, so update it if present.
                     if f is not None:
                         current_f = f
 
-                    # Calculate Distance & update coordinates
+                    # Calculate distance of the move and update coordinates.
                     dist = 0.0
                     if is_relative:
-                        # For relative moves, specified coordinates are deltas.
-                        # If a coordinate is not specified, its delta is 0.
                         dx = x if x is not None else 0.0
                         dy = y if y is not None else 0.0
                         dist = math.hypot(dx, dy)
                         current_x += dx
                         current_y += dy
-                    else:
-                        # For absolute moves, a missing coordinate means it doesn't change.
+                    else: # Absolute positioning
                         new_x = x if x is not None else current_x
                         new_y = y if y is not None else current_y
                         dist = math.hypot(new_x - current_x, new_y - current_y)
                         current_x = new_x
                         current_y = new_y
                     
-                    # Calculate Time (Minutes -> Seconds)
+                    # Convert move distance to time (seconds) and add to total.
                     if dist > 0 and current_f > 0:
                         move_time = (dist / current_f) * 60.0
                         total_time += move_time
 
-                # 3. Manage Window Size (Pruning)
-                # We only need history going back slightly further than lead_time.
-                # Adding a small safety margin (e.g. 2x lead time or +5 seconds)
+                # 3. Manage History Window Size (Pruning)
+                # To save memory, remove history entries that are older than needed.
+                # A safety margin (e.g., 2x lead_time) is added.
                 if len(history) > 0:
                     cutoff_time = total_time - (lead_time * 2.0)
-                    # Remove old entries from the left
                     while len(history) > 0 and history[0][0] < cutoff_time:
                         history.popleft()
 
         # 4. Apply Insertions
-        # Sort by Layer DESC, then Line DESC to avoid index shifting issues
+        # Sort by Layer DESC, then Line DESC to ensure inserting a command
+        # doesn't shift the indices of subsequent insertions.
         insertions.sort(key=lambda x: (x[0], x[1]), reverse=True)
 
-        # Apply
         for l_idx, ln_idx, cmd in insertions:
             layers_lines[l_idx].insert(ln_idx, cmd)
 
         # 5. Reconstruct Data
+        # Join the lines back into layer strings.
         for i in range(len(data)):
             data[i] = "\n".join(layers_lines[i])
 
         return data
+
